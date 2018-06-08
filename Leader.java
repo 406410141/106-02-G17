2c85c68ab3be6b54a549d3e626a1bfd7c2e7e42e
406410141 <39294952+406410141@users.noreply.github.com>
2018-06-08 18:55:45 +0800

    Update Leader.java

--- Leader.java
+++ Leader.java
@@ -2,7 +2,23 @@ package tetris;
 import java.sql.*;
 
 public class Leader {
+	
+	private Connection con;
+	
+    public void connect() throws Exception{
 
+        if(con != null) return;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new Exception("No database");
+        }
+
+        String connectionURL = "jdbc:mysql://localhost:3306/Peoples";
+
+        con = DriverManager.getConnection(connectionURL, "root", "0000");        
+    }
 	public void leader() {
 		// TODO Auto-generated method stub
 		
